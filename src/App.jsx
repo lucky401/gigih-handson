46ec508c2929f5031aba03250dd8b415c2ea537d
lucky404 <luckydewa123@gmail.com>
2022-03-25 19:01:34 +0700

    feat: add list of gifs

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,20 @@ import Gif from './components/gif';
 
 import { MOCK_GIF } from './constants';
 
+function gifCollection(gifs) {
+  return gifs.filter((gif) => gif.rating === 'pg');
+}
+
 function App() {
+  const gifs = gifCollection(MOCK_GIF);
+
   return (
     <div className="app">
       <h1 className="title text-center">Search GIF</h1>
       <SearchBar />
-      <Gif gif={MOCK_GIF} />
+      {gifs.map((gif) => (
+        <Gif gif={gif} key={gif.id} />
+      ))}
     </div>
   );
 }
